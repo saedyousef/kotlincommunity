dcf022b593d17ae13572f59d87875f46910038c1
Saed Yousef <s.alzabaen@classera.com>
2017-09-07 16:08:04 +0300

    Saving the views count for posts, cleaning the code, create 404 error page for posts

--- app/Model/Views.php
+++ app/Model/Views.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PostsInteration extends Model
+class Views extends Model
 {
     //
 }
---
+++ database/migrations/2017_09_07_110551_create_views.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            $table->string('device_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('views');
+    }
+}
